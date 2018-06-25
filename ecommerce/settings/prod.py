cb240a827a9f9af4257f43f9776406428cec1db3
Miguel Martinez <miguelitalia@hotmail.com>
2018-06-25 13:11:53 +0000

    obtained allowed host from heroku appstart screen and added it to prod

--- ecommerce/settings/prod.py
+++ ecommerce/settings/prod.py
@@ -4,7 +4,7 @@ import dj_database_url
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-ALLOWED_HOSTS = ['?????']
+ALLOWED_HOSTS = ['ecomerce-miguelmargar.c9users.io', 'ecommerce-learn.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
