a13b8c256e117f282c0ab8713663d890131c50aa
Miguel Martinez <miguelitalia@hotmail.com>
2018-06-26 12:07:05 +0000

    started the add to cart functionality

--- products/views.py
+++ products/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.http import HttpResponse
 # Create your views here.
 
 def get_products(request):
     products = Product.objects.all()
     return render(request, "products/products.html", {'products': products})
     
+def add_to_cart(request):
+    id = request.POST['product_id']
+    product = get_object_or_404(Product, pk=id)
+    return HttpResponse()
